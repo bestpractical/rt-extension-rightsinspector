8e117ee921a6fbe6e062a7e9fa3441da7bf999ea
Shawn M Moore <shawn@bestpractical.com>
2017-02-02 23:38:07 +0000

    Switch to RightName LIKE query, with result highlighting

--- static/js/rights-debugger.js
+++ static/js/rights-debugger.js
@@ -4,6 +4,14 @@ jQuery(function () {
         return;
     }
 
+    Handlebars.registerHelper('search_highlight', function (text, term) {
+        // this is simplistic; better would be to highlight on the
+        // unescaped text, and case insensitively
+        text = Handlebars.Utils.escapeExpression(text);
+        text = text.replace(term, '<span class="match">' + term + '</span>');
+        return new Handlebars.SafeString(text);
+    });
+
     var renderItem = Handlebars.compile(template);
     var form = jQuery('form#rights-debugger');
     var display = form.find('.results');
@@ -11,15 +19,21 @@ jQuery(function () {
     var refreshResults = function () {
         display.empty();
 
+        var serialized = form.serializeArray();
+        var search = {};
+        jQuery.each(serialized, function(i, field){
+            search[field.name] = field.value;
+        });
+
         jQuery.ajax({
             url: form.attr('action'),
-            data: form.serializeArray(),
+            data: search,
             timeout: 30000, /* 30 seconds */
             success: function (response) {
                 display.empty(); // just in case of race condition
                 var items = response.results;
                 jQuery.each(items, function (i, item) {
-                    display.append(renderItem(item));
+                    display.append(renderItem({ search: search, item: item }));
                 });
             },
             error: function (xhr, reason) {
